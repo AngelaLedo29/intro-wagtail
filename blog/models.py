7fa9c1b6f8f4ff3cb2491572812d14f2934a1290
AngelaLedo29 <angelaledo29@gmail.com>
2023-01-23 11:08:32 +0100

    Modificación de la aplicación Blog

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ from wagtail.snippets.models import register_snippet
 
 # Create your models here.
 class BlogIndexPage(Page):
+    subpage_types = ['BlogPage']
     intro = RichTextField(blank=True)
 
     #content_panels = Page.content_panels + [
@@ -22,7 +23,7 @@ class BlogIndexPage(Page):
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
+        blogpages = self.get_children().live().order_by('-latest_revision')
         context['blogpages'] = blogpages
         return context
 
@@ -38,8 +39,10 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
 
     def main_image(self):
+        'Seleciona la primera imagen de la galería'
         gallery_item = self.gallery_images.first()
         if gallery_item:
             return gallery_item.image
@@ -55,6 +58,7 @@ class BlogPage(Page):
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
         ], heading="Blog information"),
         FieldPanel('intro'),
         FieldPanel('body'),
@@ -83,3 +87,22 @@ class BlogTagIndexPage(Page):
         context = super().get_context(request)
         context['blogpages'] = blogpages
         return context
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
--- home/models.py
+++ home/models.py
@@ -9,6 +9,11 @@ class HomePage(Page):
     '''
     Añadimos campo *body* de texto enriquecido
     '''
+    # Subapage de la aplicación blog
+    #sugpage_types = ['blog.BlogIndexPage']
+
+    # No permito que tenga nuevas subpáginas
+    sugpage_types = []
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
