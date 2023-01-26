865a9e534acc8ac7cb076a8c379463f9164b9ecc
AngelaLedo29 <angelaledo29@gmail.com>
2023-01-26 12:29:48 +0100

    Modificación del Carrusel

--- home/models.py
+++ home/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.admin.edit_handlers import InlinePanel
 
 
 class HomePage(Page):
@@ -18,4 +24,18 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class HomeGalleryImage(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        #FieldPanel('image'),
+        FieldPanel('caption'),
     ]
